75f2134c82fe06821dedcefdab517b7551bddf86
Pratibha <pratibha.tomar13@gmail.com>
2020-11-01 08:26:43 +0100

    removed target blank tag for links

--- src/App.js
+++ src/App.js
@@ -18,13 +18,25 @@ export default function App() {
 
         <p>
           Open-source on{" "}
-          <a href="https://github.com/Pratibhatomar03/my-react-weather-app">
+          <a
+            href="https://github.com/Pratibhatomar03/my-react-weather-app"
+            rel="noopener noreferrer"
+          >
             GitHub{" "}
           </a>
           and hosted on{" "}
-          <a href="https://cocky-archimedes-5c9b92.netlify.app/"> Netlify </a>{" "}
+          <a
+            href="https://cocky-archimedes-5c9b92.netlify.app/"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Netlify{" "}
+          </a>{" "}
           by{" "}
-          <a href="https://ecstatic-bassi-280461.netlify.app/">
+          <a
+            href="https://ecstatic-bassi-280461.netlify.app/"
+            rel="noopener noreferrer"
+          >
             Pratibha Tomar
           </a>
         </p>
